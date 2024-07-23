a5992645f378ff55c237bc591423165bb2030a7f
Nailambe <john>
2024-07-23 22:00:07 +0300

    finish

---
+++ main.py
@@ -0,0 +1,8 @@
+from proceed_raw_api import proceed_data
+
+def main():
+    print('Nailambe (https://github.com/NailAmber)\n\nProceed raw data api configs\n')
+    proceed_data()
+
+if __name__ == '__main__':
+    main()
